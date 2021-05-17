93dc65991b4b3704a6bf4191bdb0e35cc2deed72
Viviane Souza <70662490+VivianeSouza923@users.noreply.github.com>
2021-05-16 22:11:12 -0300

    update

---
+++ correct.sql
@@ -0,0 +1,14 @@
+create database exemplo
+default character set utf8
+default collate utf8_general_ci;
+
+create table viviane(
+id int not null auto_increment,
+nome varchar(30) not null,
+nascimento date,
+sexo enum('M', 'F'),
+peso decimal(5,2),
+altura decimal(3,2),
+nacionalidade varchar(20) default 'Brasil',
+primary key(id)
+) default charset = utf8;
---
+++ insert.sql
@@ -0,0 +1,4 @@
+insert into pessoas
+(id, nome, nascimento, sexo, peso, altura, nacionalidade)
+values
+('1', 'Vivi', '1996-06-19', 'F', '47.0', '1.49', 'brasileira');
